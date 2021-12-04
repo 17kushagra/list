0dadde64dc4df6be6fe2185e3e8a11e48766f25f
kushagra sharma <2001kushagrasharma@gmail.com>
2021-12-04 22:47:45 +0530

    first

---
+++ script.js
@@ -0,0 +1,26 @@
+const clkme= document.getElementById('submitbtn');
+
+
+
+clkme.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    let today = new Date();
+    let date = String(today.getDate()+'-'+   (today.getMonth()+1)+'-'+today.getFullYear() );
+    console.log(date);
+    const text = document.getElementById('txtarea').value;
+    console.log(text)
+    const li = document.createElement('li');
+    console.log(li)
+    li.innerText=text+"("+date+")";
+    console.log(li);
+    const pp = document.getElementsByClassName('parent');
+    pp[0].appendChild(li);
+    
+
+    
+     })
+
+
+
+     
